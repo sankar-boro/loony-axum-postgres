9dd939b408fadf9e0cd046f48f039854105c23e4
Sankar Boro <sankar.boro@yahoo.com>
2024-03-06 12:06:40 +0530

    Signup

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -1,37 +1,129 @@
-mod login;
-pub use login::login;
+use axum::{
+    extract::State,
+    http::{header, HeaderMap, StatusCode},
+    response::IntoResponse, Json,
+};
+use serde_json::json;
+use crate::AppState;
+use serde::Deserialize;
+use tower_sessions::Session;
+use cookie::CookieBuilder;
+use time::Duration;
+use bcrypt::{verify, hash, DEFAULT_COST};
 
-// use serde::{Serialize, Deserialize };
-// use actix_session::Session;
-// use crate::error::Error;
+#[derive(Deserialize, Debug)]
+pub struct LoginForm {
+    username: String,
+	password: String,
+}
 
-// #[derive(Deserialize, Serialize, Debug)]
-// pub struct AUTHUSER {
-//     pub uid: i32,
-//     pub fname: String,
-//     pub lname: String,
-//     pub email: String,
-// }
+#[derive(Deserialize, Debug)]
+pub struct SignupForm {
+	username: String,
+	password: String,
+    fname: String,
+    lname: String
+}
 
-// #[allow(dead_code)]
-// pub fn auth_session(session: &Session) -> Result<AUTHUSER, Error>  {
-//     let auth_user = session.get::<String>("AUTH_USER")?;
-//     match auth_user {
-//         Some(auth_user) => Ok(serde_json::from_str(&auth_user)?),
-//         None => return Err(Error::from("UN_AUTHENTICATED_USER").into())
-//     }
-// }
 
-// pub trait AuthSession {
-//     fn user_info(&self) -> Result<AUTHUSER, Error>;
-// }
+fn internal_error<E>(err: E) -> (StatusCode, Json<serde_json::Value>)
+where
+    E: std::error::Error,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, Json(json!({
+		"message": err.to_string(),
+	})))
+}
 
-// impl  AuthSession for Session {
-//     fn user_info(&self) -> Result<AUTHUSER, Error> {
-//         let auth_user = self.get::<String>("AUTH_USER")?;
-//         match auth_user {
-//             Some(auth_user) => Ok(serde_json::from_str(&auth_user)?),
-//             None => return Err(Error::from("UN_AUTHENTICATED_USER").into())
-//         }
-//     }
-// }
+
+pub async fn login(
+	State(pool): State<AppState>,
+	_: Session,
+    Json(body): Json<LoginForm>,
+) 
+-> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)>
+{
+	// request.validate()?;
+	let conn = pool.pg_pool.get().await.map_err(internal_error)?;
+	let row = conn
+        .query_one("select user_id, fname, lname, password from users where phone=$1", &[&body.username])
+        .await
+        .map_err(internal_error)?;
+	
+	let user_id: i32 = row.get(0);
+	let fname: String = row.get(1);
+	let lname: String = row.get(2);
+	let password: String = row.get(3);
+
+	let is_valid_password = verify(&body.password, &password).map_err(|err| { (StatusCode::INTERNAL_SERVER_ERROR, Json(json!({
+		"message": err.to_string(),
+	}))) })?;
+
+	if !is_valid_password {
+		return Err((StatusCode::INTERNAL_SERVER_ERROR, Json(json!({
+			"message": "Invalid password".to_string(),
+		}))));
+	}
+
+	let user_response = json!({
+		"userId": user_id,
+		"phone": &body.username.clone(),
+		"fname": fname.clone(),
+		"lname": lname.clone(),
+	});
+
+    
+	let cookie = CookieBuilder::new("Authorization", "sankar")
+	.domain("http://localhost:3000")
+	.path("/")
+	.secure(true)  // Set to true for HTTPS only
+	.http_only(true) // Set to true to prevent JavaScript access
+	.max_age(Duration::seconds(30)) // Set cookie expiration
+	.build().to_string();
+
+
+	let mut header_map = HeaderMap::new();
+    header_map.insert(header::SET_COOKIE, cookie.parse().unwrap());
+
+	Ok((
+		StatusCode::OK,
+		[(header::CONTENT_TYPE, "application/json")],
+		header_map,
+		Json(user_response),
+	))
+}
+
+
+pub async fn signup(
+	State(pool): State<AppState>,
+	_: Session,
+    Json(body): Json<SignupForm>,
+) 
+-> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)>
+{
+	// request.validate()?;
+	let conn = pool.pg_pool.get().await.map_err(internal_error)?;
+
+    let hashed_password= hash(&body.password, DEFAULT_COST).map_err(|err| {(StatusCode::INTERNAL_SERVER_ERROR, Json(json!({
+        "message": err.to_string(),
+    }))) })?;
+	let _ = conn
+        .query("INSERT INTO users(username, password, fname, lname) values($1, $2, $3, $4)", &[&body.username, &hashed_password, &body.fname, &body.lname])
+        .await
+        .map_err(internal_error)?;
+	
+	let user_response = json!({
+		"username": &body.username.clone(),
+		"fname": &body.fname.clone(),
+		"lname": &body.lname.clone(),
+	});
+
+	Ok((
+		StatusCode::OK,
+		[(header::CONTENT_TYPE, "application/json")],
+		// header_map,
+		Json(user_response),
+	))
+}
+
+
--- src/route.rs
+++ src/route.rs
@@ -8,11 +8,12 @@ use tower_sessions::{Expiry, SessionManagerLayer};
 use tower_sessions_redis_store::{fred::prelude::*, RedisStore};
 use crate::AppState;
 
-use crate::auth::login;
+use crate::auth::{login, signup};
 
 
 pub async fn create_router(connection: AppState, cors: CorsLayer) -> Router {
-    let auth = Router::new().route("/login", get(login).post(login));
+    let auth = Router::new().route("/login", get(login).post(login))
+    .route("/signup", post(signup));
     let pool = RedisPool::new(RedisConfig::from_url("redis://:sankar@127.0.0.1:6379/").unwrap(), None, None, None, 6).unwrap();
 
     pool.connect();
