fe0ad9a62bdf806ca9bcba0407c7aec1a17da1a6
Sankar Boro <sankar.boro@yahoo.com>
2024-03-27 12:39:46 +0530

    Error implementattions

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,14 @@
-use axum::{http::StatusCode, Json};
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    Json,
+};
 use serde_json::json;
+
+pub enum AppError {
+    // NotFound(String),
+    InternalServerError(String),
+}
 
 pub fn internal_error<E>(err: E) -> (StatusCode, Json<serde_json::Value>)
 where
@@ -11,4 +20,32 @@ where
             "message": err.to_string(),
         })),
     )
+}
+
+impl From<std::io::Error> for AppError {
+    fn from(err: std::io::Error) -> Self {
+        AppError::InternalServerError(err.to_string())
+    }
+}
+
+impl From<tokio_postgres::Error> for AppError {
+    fn from(err: tokio_postgres::Error) -> Self {
+        AppError::InternalServerError(err.to_string())
+    }
+}
+
+impl From<bb8::RunError<tokio_postgres::Error>> for AppError {
+    fn from(err: bb8::RunError<tokio_postgres::Error>) -> Self {
+        AppError::InternalServerError(err.to_string())
+    }
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        let body = match self {
+            // AppError::NotFound(e) => e,
+            AppError::InternalServerError(e) => e,
+        };
+        (StatusCode::INTERNAL_SERVER_ERROR, body).into_response()
+    }
 }
