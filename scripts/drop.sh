32c9b641ab95c8e3ddd987edd920f80cbd3888ca
Sankar Boro <sankar.boro@yahoo.com>
2025-02-23 12:11:16 +0530

    Changes in script

--- scripts/drop.sh
+++ scripts/drop.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-PWD=$(pwd)
-dropfile=$PWD/db/postgres/drop.sql
+if [ "$#" -ne 2 ]; then
+  echo "Usage: ./scripts/create_tables.sh <user_name> <db_name>"
+  exit 0
+fi
 
-psql -h localhost -U sankar -d sankar -f $dropfile -W
+# Set environment (dev or prod)
+user_name=${1}
+db_name=${2}
+
+PWD=$(pwd)
+dropfile=$PWD/db/postgres/drop_all_tables.sql
+
+psql -h localhost -U $user_name -d $db_name -f $dropfile -W
 
